4a50a8b5788dbb3f31892600a7ed05aace7e47ba
megbug <meghub@proton.me>
2023-04-07 10:42:02 +0200

    initial commit

---
+++ src/Components/Firstsection.js
@@ -0,0 +1,32 @@
+const Firstsection = () => {
+    return (
+        <div className="first-section">
+            <h3>What We Do</h3>
+            <h4>We've got everything you need to launch and grow your business</h4>
+            <section className="four">
+                <div>
+                    <img src="./img/brush.svg" alt="" />
+                    <h3>Brand Identity</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, perferendis? Illum, minima obcaecati? Inventore ullam voluptas vero sapiente repudiandae ipsam. Perferendis reiciendis ut neque qui pariatur eveniet repudiandae labore architecto?</p>
+                </div>
+                <div>
+                    <img src="./img/object-select.svg" alt="" />
+                    <h3>Illustration</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, perferendis? Illum, minima obcaecati? Inventore ullam voluptas vero sapiente repudiandae ipsam. Perferendis reiciendis ut neque qui pariatur eveniet repudiandae labore architecto?</p>
+                </div>
+                <div>
+                    <img src="./img/megaphone.svg" alt="" />
+                    <h3>Marketing</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, perferendis? Illum, minima obcaecati? Inventore ullam voluptas vero sapiente repudiandae ipsam. Perferendis reiciendis ut neque qui pariatur eveniet repudiandae labore architecto?</p>
+                </div>
+                <div>
+                    <img src="./img/globe-line.svg" alt="" />
+                    <h3>Web Design</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, perferendis? Illum, minima obcaecati? Inventore ullam voluptas vero sapiente repudiandae ipsam. Perferendis reiciendis ut neque qui pariatur eveniet repudiandae labore architecto?</p>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Firstsection;
---
+++ src/Components/Head.js
@@ -0,0 +1,30 @@
+const Head = () => {
+    return (
+        <div className="Header">
+            <h2>Hello There</h2>
+            <h1>We are Glint</h1>
+            <hr />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed dolorem, libero ex quo, ipsum odio officia doloremque sapiente cumque expedita necessitatibus praesentium fugit exercitationem qui ratione quia nisi, nobis perferendis.</p>
+            <section className="DivSection">
+                <div>
+                    <p>127</p>
+                    <p>Award Received</p>
+                </div>
+                <div>
+                    <p>1505</p>
+                    <p>Cups of Coffee</p>
+                </div>
+                <div>
+                    <p>109</p>
+                    <p>Projecst Completed</p>
+                </div>
+                <div>
+                    <p>102</p>
+                    <p>Happy Clients</p>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Head;
